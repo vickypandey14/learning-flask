2bbb724458c64ab86de6b5900caf4a5cdf73da0c
Vivek Pandey <vickypandeyts@gmail.com>
2025-04-21 23:00:57 +0530

    learned about flask routing at beginner level

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World! My name is vivek and i am learning flask!'
+
+@app.route('/about')
+def about():
+    return 'This is the about page of the Flask application.'
+
+# Dynamic route example
+
+@app.route('/user/<username>/<age>')
+def user_profile(username , age):
+    return f'User profile page for {username} with age {age}.'
+
+# GET method example
+
+@app.route('/post/<int:post_id>', methods=['GET'])
+def show_post(post_id):
+    return f'Post: {post_id}'
+
+# POST and GET method example
+
+@app.route('/submit', methods=['GET','POST'])
+def submit_form():
+    if request.method == 'POST':
+        return 'Your Form has been submitted!'
+    else:
+        return 'Please try to submit the form again!'
 
 if __name__ == '__main__':
     app.run(debug=True)
